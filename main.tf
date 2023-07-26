036ca42dad6db4bd7c459f561e25dc4ab9488d88
Mukesh <Stunty@007>
2023-07-26 22:14:26 +0530

    learning devops

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 module "vpc" {
-  for_each                  = var.vpc
-  source                    = "./vendor/modules/vpc"
-  cidr_block                = each.value.cidr_block
-  additional_cidr_block     = each.value.additional_cidr_block
-  private_subnets           = each.value.private_subnets
-  public_subnets            = each.value.public_subnets
-  subnet_availability_zones = each.value.subnet_availability_zones
-  env                       = var.env
-  management_vpc            = var.management_vpc
-#  private_zone_id           = var.private_zone_id
+  source = "./vendor/modules/vpc"
+  vpc    = var.vpc
+  env    = var.env
+  source         = "./vendor/modules/vpc"
+  vpc            = var.vpc
+  env            = var.env
+  management_vpc = var.management_vpc
 }
