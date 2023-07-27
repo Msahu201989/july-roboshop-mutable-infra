4ea47ff84874418edce6acffc17073bbe6e100a7
Mukesh <Stunty@007>
2023-07-27 21:51:58 +0530

    July 23 learning devops

--- locals.tf
+++ locals.tf
@@ -1,2 +1,6 @@
 locals {
-  app_private_subnets = lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null), "app", null), "subnets", null)}
+  app_private_subnets = lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null), "app", null), "subnets", null)
+
+  database_private_subnets = lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null), "database", null), "subnets", null)
+
+}
