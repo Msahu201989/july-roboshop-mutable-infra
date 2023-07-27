fe3177dae271ef08bc7059380b0e04c0bbf24c9f
Mukesh <Stunty@007>
2023-07-27 21:43:55 +0530

    July 23 learning devops

--- main.tf
+++ main.tf
@@ -18,5 +18,5 @@ module "vpc" {
 #}
 
 output "app_private_subnets" {
-  value = lookup(lookup(local.private_subnets, "subnets",null), "apps", null)
+  value = lookup(lookup(local.private_subnets, "subnets",null), "app", null)
 }
